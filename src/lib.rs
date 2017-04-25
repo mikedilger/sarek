519601dea17eb2d5d66594691e04f750aecae883
Mike Dilger <mike@optcomp.nz>
2017-04-26 10:29:10 +1200

    Depend on (for windows) winapi, user32

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,8 @@ extern crate vk_sys;
 extern crate winit;
 extern crate image;
 extern crate libc;
+#[cfg(windows)] extern crate user32;
+#[cfg(windows)] extern crate winapi;
 
 #[cfg(test)]
 mod tests;
