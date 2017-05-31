dbb62f2e7357a99a9cec8ea941deb6280eb9b57a
Mike Dilger <mike@optcomp.nz>
2017-05-31 14:30:55 +1200

    Enhance Error type (impl std::error::Error)

--- src/error.rs
+++ src/error.rs
@@ -1,5 +1,8 @@
 
+use std::error::Error as StdError;
 use std::convert::From;
+use std::fmt;
+
 use std::ffi::NulError;
 use std::str::Utf8Error;
 use vks::VkResult;
@@ -10,6 +13,37 @@ pub enum Error {
     Nul(NulError),
     Vulkan(VkResult),
     StrUtf8(Utf8Error),
+}
+
+impl StdError for Error {
+    fn description(&self) -> &str
+    {
+        match *self {
+            Error::General(_) => "",
+            Error::Nul(_) => "Nul Error",
+            Error::Vulkan(_) => "Vulkan Error",
+            Error::StrUtf8(_) => "UTF-8 Error",
+        }
+    }
+
+    fn cause(&self) -> Option<&StdError> {
+        match *self {
+            Error::Nul(ref e) => Some(e),
+            Error::StrUtf8(ref e) => Some(e),
+            _ => None,
+        }
+    }
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            Error::General(ref s) => write!(f, "{}", s),
+            Error::Nul(ref e) => write!(f, "{}: {}", self.description(), e),
+            Error::Vulkan(ref e) => write!(f, "{}: {:?}", self.description(), e),
+            Error::StrUtf8(ref e) => write!(f, "{}: {}", self.description(), e),
+        }
+    }
 }
 
 impl From<String> for Error {
