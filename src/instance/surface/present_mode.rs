b119171715e7eb9c146482e34e9808c1a9f58e2e
Mike Dilger <mike@optcomp.nz>
2017-06-12 16:16:54 +1200

    PresentMode as enum

--- src/instance/surface/present_mode.rs
+++ src/instance/surface/present_mode.rs
@@ -1,17 +1,38 @@
 
+use std::mem;
 use vks::VkPresentModeKHR;
 
-#[derive(Debug, Clone)]
-pub struct PresentMode(VkPresentModeKHR); // u32
+#[repr(u32)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
+pub enum PresentMode {
+    Immediate = 0,
+    Mailbox = 1,
+    Fifo = 2,
+    FifoRelaxed = 3,
+    #[cfg(feature = "khr_shared_presentable_image")]
+    SharedDemandRefresh = 1000111000,
+    #[cfg(feature = "khr_shared_presentable_image")]
+    SharedContinuousRefresh = 1000111001,
+}
 
 impl From<VkPresentModeKHR> for PresentMode {
     fn from(vk: VkPresentModeKHR) -> PresentMode {
-        PresentMode(vk)
+        unsafe {
+            mem::transmute(vk.as_raw())
+        }
     }
 }
 
 impl Into<VkPresentModeKHR> for PresentMode {
     fn into(self) -> VkPresentModeKHR {
-        self.0
+        VkPresentModeKHR::from_raw(unsafe {
+            mem::transmute(self)
+        })
+    }
+}
+
+impl Default for PresentMode {
+    fn default() -> PresentMode {
+        PresentMode::Immediate
     }
 }
